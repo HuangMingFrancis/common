b9c23e7b7375302f41a645582f3e5974b9754ab2
HuangMingFrancis <2967134464@qq.com>
2017-12-10 10:27:54 +0800

    增加DiskLruCache处理类

---
+++ src/main/java/com/francis/common/utils/file_util/FileUtil.java
@@ -0,0 +1,55 @@
+package com.francis.common.utils.file_util;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Francis on 2017/12/9.
+ * 文件处理类
+ */
+
+public class FileUtil {
+
+    //获取缓存地址,判断手机是否有SD卡
+    public static File getDiskCacheDir(Context context, String uniqueName) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
+
+    //diskLruCache将图片地址转换为MD5加密的Key
+    public static String hashKeyForDisk(String key) {
+        String cacheKey;
+        try {
+            final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+            mDigest.update(key.getBytes());
+            cacheKey = bytesToHexString(mDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            cacheKey = String.valueOf(key.hashCode());
+        }
+        return cacheKey;
+    }
+
+    private static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                sb.append('0');
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+
+}
